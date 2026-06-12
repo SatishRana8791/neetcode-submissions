04814328da982263cd3f574ca418b9bdf9eb0d65
Satish Kumar <satishrana8791@gmail.com>
2026-06-12 11:46:32 +0530

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int n=s.length();
+        stack<int>st1;  //for left parenthesis
+        stack<int>st2;  //for right parenthesis
+
+        for(int i=0;i<n;i++){
+            if(s[i]=='(') st1.push(i);
+            else if( s[i]=='*') st2.push(i);
+            else{
+                if(s[i]==')' && !st1.empty() ){
+                    st1.pop();
+                }
+                else if(s[i]==')' && !st2.empty() ){
+                    st2.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            
+        }
+        while(!st1.empty() && !st2.empty()){
+            if(st1.top() > st2.top()){
+                return false;
+            }
+            st1.pop();
+            st2.pop();
+        }
+        return st1.size() <= st2.size();
+    }
+};
