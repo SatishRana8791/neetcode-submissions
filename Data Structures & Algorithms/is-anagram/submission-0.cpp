b1d16538fc6a1e74ff3930b8ee339a2b023a7eb3
Satish Kumar <satishrana8791@gmail.com>
2026-07-13 15:19:12 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n1=s.length();
+        int n2=t.length();
+        if(n1 != n2) return false;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
+    }
+};
